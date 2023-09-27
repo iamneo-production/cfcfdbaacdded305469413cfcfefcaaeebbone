cd479bcf18943312a02db4925aa23dd0948acaf0
Iamneo <techadmin@iamneo.ai>
2023-09-27 06:36:25 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -4,10 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <form id="data-test">
-        <label id="data-test"> </label>
-        <button id="data-test"></button>
-      </form>
+      <div className="contents">
+        <h3>Hey!!! Greeting</h3>
+        <form onSubmit={handleSubmit} data-testid="form">
+          <label data-testid = "label">Enter name : </label>
+          <input type="text" value={name} onChange={handleChange}/>
+          <button type="submit" data-testid = "buttonElement"></button>
+        </form>
+      </div>
     </div>
   );
 }
