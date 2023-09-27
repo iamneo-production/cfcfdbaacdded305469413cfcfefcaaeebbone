1fd1cc7eb8f1179625b2102d25b4f86c98fa4aa1
Iamneo <techadmin@iamneo.ai>
2023-09-27 06:41:25 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -9,8 +9,9 @@ function App() {
         <form onSubmit={handleSubmit} data-testid="form">
           <label data-testid = "label">Enter name : </label>
           <input type="text" value={name} onChange={handleChange}/>
-          <button type="submit" data-testid = "buttonElement"></button>
+          <button type="submit" data-testid = "buttonElement">Say Hello</button>
         </form>
+        {greeting && <h2>{greeting}</h2>}
       </div>
     </div>
   );
